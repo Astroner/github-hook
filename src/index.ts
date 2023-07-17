23f32968ae519204543049a0402095d9511e2dcc
Evgeny Nikiforov <enikiforov@devexperts.com>
2023-07-18 00:04:36 +0300

    pre publish

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import * as parse from "minimist";
-import { fork } from "child_process";
-
 export { createRouter, RouterConfig } from "./create-router";
 export { createHook, HookConfig } from "./create-hook";
